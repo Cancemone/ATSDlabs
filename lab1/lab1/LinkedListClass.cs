5659e8098a517c6de75ed102f547361239bdcb2d
DESKTOP-07IK3VB\Демид <demyd.vnukov@nure.ua>
2021-03-23 18:46:59 +0200

    Added Linked List class

---
+++ lab1/lab1/LinkedListClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    class LinkedListClass <T> where T : IComparable
+    {
+        private LinkedListNode<T> head;
+        private LinkedListNode<T> tail;
+
+        private int count;
+    }
+}

cb009cb63c1772b12c9702b413505d0b57e193db
DESKTOP-07IK3VB\Демид <demyd.vnukov@nure.ua>
2021-03-23 10:58:02 +0200

    Created Linked list node

---
+++ lab1/lab1/LinkedListNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class LinkedListNode<T>
+    {
+        //значение в узле списка
+        public T Value;
+        //следующий узел
+        public LinkedListNode<T> Next;
+        //constructor
+        public LinkedListNode(T value, LinkedListNode<T> next = null)
+        {
+            Value = value;
+            Next = next;
+        }
+    }
+}

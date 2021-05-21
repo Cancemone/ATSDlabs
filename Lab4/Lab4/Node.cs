dd0e46b89c8573481a97e24f813b65430f9a3e00
DESKTOP-07IK3VB\Демид <demyd.vnukov@nure.ua>
2021-05-21 18:09:38 +0300

    Added Node.cs

---
+++ Lab4/Lab4/Node.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    class Node<T>
+    {
+        public T Data;
+        public Node<T> Next;
+        public Node(T data)
+        {
+            Data = data;
+        }
+        public void setItem(T i)
+        {
+            Data = i;
+        }
+        public void setNext(Node<T> n)
+        {
+            Next = n;
+        }
+    }
+}
